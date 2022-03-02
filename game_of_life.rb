a2a2a3d321cb78d7248107deb2f26383d355adf3
kjdchapman <kjdchapman@gmail.com>
2022-03-02 13:41:10 +0000

    Skeleton for second acceptance spec (first spec now passing)

--- game_of_life.rb
+++ game_of_life.rb
@@ -4,9 +4,14 @@ class GameOfLife
 
     new_grid.each do |row|
       row.each do |cell|
-        living_neighbours_count(cell, new_grid)
+        # living_neighbours_count(cell, new_grid)
+        living_neighbours_count = 0
         new_state_of_cell(cell, living_neighbours_count)
       end
     end
+  end
+
+  def new_state_of_cell(cell, living_neighbours_count)
+    cell
   end
 end
